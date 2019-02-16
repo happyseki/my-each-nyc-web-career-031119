5be184c6a86559050f9ec11b140f22f60cf2f3bf
Elaina <elainaseki@gmail.com>
2019-02-16 02:44:30 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,10 +1,12 @@
 def my_each(collection) # put argument(s) here
   i = 0
   while i < collection.length
-    yield collection[i]
+    yield(collection[i])
      i += 1
   end
    return collection
   # code here
 end
+
+
 
